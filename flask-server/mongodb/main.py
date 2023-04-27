b3de73df362dfad95b89de145bb0dad1ec56afb6
Bogdan26Bogdan <49450616+Bogdan26Bogdan@users.noreply.github.com>
2023-04-26 23:20:45 -0700

    mongoDB connection works
    Basic funcionality works for adding documents and viewing collections

--- flask-server/mongodb/main.py
+++ flask-server/mongodb/main.py
@@ -7,6 +7,23 @@ password = os.environ.get("password")
 
 import database
 db = database.Database(username, password)
+
+
+
+import sys
+script_dir = os.path.dirname( __file__ )
+mymodule_dir = os.path.join( script_dir, '..', ".." , "classes")
+sys.path.append( mymodule_dir )
+import printer
+
+
+
+x = printer.Printer("1", "Working")
+print(f"Object id of the newly added printer: {db.add_value(x)}")
+print(db.find_object(x.data, {"ID": "1"}))
+
+
+
 print(db.get_all_collections())
 
 
