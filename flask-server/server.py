79cbe769610fcfc194c127d1b62e6b624163c448
Bogdan26Bogdan <49450616+Bogdan26Bogdan@users.noreply.github.com>
2023-04-27 00:09:08 -0700

    Capped file size at 15MB

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,6 +2,9 @@ from flask import Flask, send_from_directory
 
 
 app= Flask(__name__)
+#Cap file size at 15MB 
+app.config['MAX_CONTENT_LENGTH'] = 15 * 1000 * 1000
+
 
 @app.route("/")
 def hello():
