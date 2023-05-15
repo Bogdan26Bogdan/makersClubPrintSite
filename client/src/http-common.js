459fe658ea934a716ec89af91f6a7b3a6b3a2545
KyleTheMaker <94996595+KyleTheMaker@users.noreply.github.com>
2023-05-15 16:16:08 -0700

    updates files to most recent dev branch merge

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default axios.create({
     //base URL is the location that Vue communicates to
     //This will need to be updated to communicat with FLASK - on port 5000?
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:5000/",
     headers: {
         "Content-Type": "application/json"
     }
